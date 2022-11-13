fdafcd8d5ba2a408c6548783d1935e75a655ae2f
DawidNst <https://github.com/DawidNst/Java-Lombok.git>
2022-11-13 11:47:19 +0100

    19,2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -15,21 +15,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
 
     private final DbService service;
     private final TaskMapper taskMapper;
 
-    @Autowired
-    public TaskController(DbService service, TaskMapper taskMapper) {
-        this.service = service;
-        this.taskMapper = taskMapper;
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
-
-      @GetMapping
-        public List<TaskDto> getTasks() {
-            return new ArrayList<>();
-        }
         @GetMapping ( value = "{taskId}")
         public TaskDto getTask(@PathVariable Long taskId) {
             return new TaskDto(1L, "test title", "test_content");

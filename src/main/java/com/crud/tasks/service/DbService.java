698179aff8480f838b78d35f1749775a1ab7e852
DawidNst <https://github.com/DawidNst/Java-Lombok.git>
2022-11-13 11:44:53 +0100

    19,2

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,11 +1,13 @@
 package com.crud.tasks.service;
 
+
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +15,19 @@ public class DbService {
 
     private final TaskRepository repository;
 
+
     public List<Task> getAllTasks() {
         return repository.findAll();
     }
+
+
+    public Optional<Task> getTask(final Long taskId) {
+        return repository.findById(taskId);
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+
 }

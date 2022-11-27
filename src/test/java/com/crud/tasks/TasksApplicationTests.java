7dc45e859a9ac771ee77f21b0166ffdca1ff1db7
DawidNst <dawidniesterczuk@gmail.com>
2022-11-27 12:36:37 +0100

    19,3

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -1,9 +1,5 @@
 package com.crud.tasks;
-
-import com.crud.tasks.domain.TaskDto;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest

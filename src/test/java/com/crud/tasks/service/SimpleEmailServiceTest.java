41228431cff1b6501c52937c12eb619e5c716ee9
DawidNst <dawidniesterczuk@gmail.com>
2022-12-12 12:04:50 +0100

    23,3

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -25,16 +25,18 @@ class SimpleEmailServiceTest {
     @Test
     public void shouldSendEmail() {
         //Given
-        Mail mail = new Mail("test@test.com", "Test", "Test Message");
-
+        // Mail mail = new Mail("test@test.com", "Test", "Test Message", null);
+        Mail mail = Mail.builder()
+                .mailTo("test@test.com")
+                .subject("Test")
+                .message("Test Message")
+                .build();
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-
         //When
         simpleEmailService.send(mail);
-
         //Then
         verify(javaMailSender, times(1)).send(mailMessage);
     }

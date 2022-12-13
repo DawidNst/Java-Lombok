c665615d5a87d5373e9f8484022c50036051e2b9
DawidNst <dawidniesterczuk@gmail.com>
2022-12-13 19:00:22 +0100

    23,4

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,20 +1,17 @@
 package com.crud.tasks;
 
-import com.crud.tasks.domain.TaskDto;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
-@SpringBootApplication
 @OpenAPIDefinition
-
+@EnableScheduling
+@SpringBootApplication
 public class TasksApplication {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(TasksApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(TasksApplication.class, args);
 	}
 
-}
 
+}
--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -3,6 +3,7 @@ package com.crud.tasks.domain;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Builder
 @Getter
@@ -11,4 +12,5 @@ public class Mail {
     private final String subject;
     private final String message;
     private final String toCc;
+
 }

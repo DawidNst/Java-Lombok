ac8cad6d719e01442f5ce65cbcaff56f61f59b0f
DawidNst <dawidniesterczuk@gmail.com>
2023-02-21 21:45:54 +0100

    29,2

--- src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
+++ src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
@@ -5,12 +5,9 @@ import com.crud.tasks.mapper.TrelloMapper;
 import com.crud.tasks.service.TrelloService;
 import com.crud.tasks.trello.validator.TrelloValidator;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
